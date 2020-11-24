addb91bfe31b215b19a8e108ac2c52d5fc61f181
SohrabAmin <sohrobo@hotmail.com>
2020-11-24 15:16:55 -0500

    Start fresh, new file.

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 import rumps
 
-class AwesomeStatusBarApp(rumps.App):
+
+class NotionMenuBar(rumps.App):
     def __init__(self):
-        super(AwesomeStatusBarApp, self).__init__("Awesome App", icon="notion-logo.png")
-        self.menu = ["Preferences", "Silly button", "Say hi", "Send to Notion"]
+        super(NotionMenuBar, self).__init__("Notion Menu Bar",  icon="notion-logo.png")
+        # self.add_to = rumps.MenuItem(title="Add To")
+        # self.add_database = rumps.MenuItem(title="Add Database")
+        self.databases = {}
+        # self.menu = [self.add_to, self.add_database]
 
-    @rumps.clicked("Preferences")
-    def prefs(self, _):
-        rumps.alert("jk! no preferences available!")
-
-    @rumps.clicked("Silly button")
-    def onoff(self, sender):
-        sender.state = not sender.state
-
-    @rumps.clicked("Say hi")
-    def sayhi(self, _):
-        rumps.notification("Awesome title", "amazing subtitle", "hi!!1")
-
-    @rumps.clicked("Send to Notion")
-    def send(self, _):
-        window = rumps.Window(title="Send Link to Notion", default_text="https://...", ok="Send to Notion", dimensions=(100, 100))
+    @rumps.clicked("Add Database")
+    def get_database(self, _):
+        window = rumps.Window(title="Add The Database Block Link",
+                              cancel="Cancel", ok="Save Database",
+                              dimensions=(250, 100))
         window.icon = 'notion-logo.png'
         window = window.run()
         if window.clicked:
-            print(window.text)
+            self.databases[window.text] = rumps.MenuItem(title=window.text)
+            self.menu['Add To'].update(self.databases)
+
 
 
 if __name__ == "__main__":
-    AwesomeStatusBarApp().run()
+    app = NotionMenuBar()
+    app.menu = [
+        {
+            'Add To': {
+
+            }
+        },
+        None,
+        'Add Database',
+        None
+    ]
+    app.run()
